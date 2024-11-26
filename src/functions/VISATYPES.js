b09695d05ec2077fe69d924f9e0920596c79d86d
mohamedfahim1045 <mohamedfahim1045@gmail.com>
2024-11-26 21:22:54 +0200

    update the project

---
+++ src/functions/VISATYPES.js
@@ -0,0 +1,23 @@
+export const VISATYPES =
+    [
+        {
+            name: 'Tourist Visa',
+            id: 'tourist visa',
+        },
+        {
+            name: 'Business Visa',
+            id: 'business visa',
+        },
+        {
+            name: 'Visit Visa',
+            id: 'visit visa',
+        },
+        {
+            name: 'Transferable Iqama',
+            id: 'transferable iqama',
+        },
+        {
+            name: 'Non-Transferable Iqama',
+            id: 'non-transferable iqama',
+        },
+    ];
